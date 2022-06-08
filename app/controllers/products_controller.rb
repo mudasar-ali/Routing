28efdb2266f9666625ef1f9983be22484a487976
mudassar-ali <mudassar.ali@devsinc.com>
2022-06-08 18:27:12 +0500

    Can create new product and save it to database

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,25 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
+
   def show
     @product = Product.find(params[:id])
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get       'products',      to: "products#index",      as: "products"
-  #verb      URI           controller+action             prefix(products_path)
-  get 'products/:id', to: "products#show", as: "product"
+  # get       'products',      to: "products#index",      as: "products"
+  # #verb      URI           controller+action             prefix(products_path)
+  # get 'products/:id', to: "products#show", as: "product"
+  resources :products
+
 
 end
